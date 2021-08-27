b4efe63ce5c319462ac4d82c70cdf02837c8c1ab
Evan Nguyen <59784844+nguyenevan42@users.noreply.github.com>
2021-08-27 00:44:58 -0700

    Create project
    WPF project, skeleton user.cs, see comments

---
+++ TimePunch/user.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class User
+{
+	int employeeId;
+	int password;
+	int isAdmin;
+	int payRate;
+
+	private static int StartShfit(employeeId)
+	{
+
+	}
+
+	private static int EndShift(employeeId)
+    {
+
+    }
+
+	private static int StartLunch(employeeId)
+    {
+
+    }
+
+	private static int EndLunch(employeeId)
+    {
+
+    }
+
+	private static int ChangePass(password)
+    {
+
+    }
+
+	private static int ChangeAdmin(isAdmin)
+    {
+
+    }
+
+    // From my understanding, I don't think we take the arguments ID, PW, and payrate here 
+    // Wouldn't taking these pull from the locally created instance of User?
+    // Same with ChangePayRate, if we change pay rate I think we would be changing the current users payrate, not the target
+	private static int CreateUser(isAdmin, employeeId, password, payRate) 
+    {
+
+    }
+
+	private static int ChangePayRate(isAdmin, PayRate)
+    {
+
+    }
+}
