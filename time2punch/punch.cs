f13c367e964da1f4d87196ec098aaa609c1b22cf
Evan Nguyen <59784844+nguyenevan42@users.noreply.github.com>
2021-08-31 21:45:37 -0700

    newly structured
    new soln, fixed some stuff

---
+++ time2punch/punch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimePunch
+{
+    class punch
+    {
+        int punchType { get; set; }
+        int punchId { get; set; }
+        DateTime dateTime { get; set; }
+
+        public static DateTime getDateTime()
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("time:" + dateTime);
+
+            return dateTime;
+        }
+    }
+}
---
+++ time2punch/user.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace TimePunch
+{
+    public class user
+    {
+
+        
+        
+        private int employeeId;
+        private string password;
+        float payRate;
+
+        public user()
+        {
+            employeeId = 000;
+            password = "none";
+            payRate = 0.0f;
+
+        }
+
+        private static int StartShift()
+        {
+            return 0;
+        }
+
+        private static int EndShift()
+        {
+            return 0;
+        }
+
+        private static int StartLunch()
+        {
+            return 0;
+        }
+
+        private static int EndLunch()
+        {
+            return 0;
+        }
+
+        private static int ChangePass()
+        {
+            return 0;
+        }
+
+        class Admin : user
+        {
+
+            public Admin()
+            {
+
+            }
+            /*
+                    private void changePunch(punchId, employeeId, punchTime)
+                    {
+
+                    }
+
+                    private static int changePass(employeeId, password)
+                    {
+                        return 0;
+                    }
+                    private static int changeAdmin(employeeId, isAdmin)
+                    {
+                        return 0;
+                    }
+                    private static int createUser(isAdmin, employeeId, password, payRate)
+                    {
+                        return 0;
+                    }
+                    private static int changePayRate(isAdmin, PayRate)
+                    {
+                        return 0;
+                    }
+            */
+        }
+    }
+}
